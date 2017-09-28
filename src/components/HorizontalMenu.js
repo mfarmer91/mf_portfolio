4a0d2e57b8ec217209277c564cafb4cc223cc40b
Michael Farmer <mfarmer91@gmail.com>
2017-09-28 13:30:07 -0400

    added img and vertical text

--- src/components/HorizontalMenu.js
+++ src/components/HorizontalMenu.js
@@ -5,8 +5,17 @@ const HorizontalMenu = () => (
     <div id='horizontalmenu'>
         <div id='horizontalmenu_ctn' className="ui top attached segment">
             <h1 className='ui right aligned header'>Hi!<i className="em em-raised_hand"></i> I'm Michael.
-              <div className="sub header">The answer is yes.  I'll happily build your next website for you.</div>
-    </h1>
+                <div className="sub header">And yes, I will build your next website.</div>
+            </h1>
+            <div className='container'>
+                <div id='vertical-text' className='description'>mfarmer91@gmail.com<i id='arrow' className="em em-arrow_left"></i></div>
+                <div id='img_card' className="ui card">
+                  <div className="image">
+                    <img src={require("../images/mf-head.png")} />
+                  </div>
+                </div>
+            </div>
+
         </div>
         <div className="ui bottom attached tabular menu">
           <a className="active item">
